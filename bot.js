75f4b82298bda039b0822d2016fecd3442beb9ea
cekarinzahar44-del <cekarinzahar44@gmail.com>
2026-05-13 20:16:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+// 🔓 ОТКЛЮЧАЕМ ПРОВЕРКУ SSL СЕРТИФИКАТОВ (ФИКС ДЛЯ ХОСТИНГА)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
 const http = require('http'); // ВНИМАНИЕ: Здесь HTTP, а не express!
